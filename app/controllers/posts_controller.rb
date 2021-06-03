35a34052e8b277882d1eb679f3a19917ddebcba7
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-06-03 21:54:35 +0900

    共通処理をメソッド化しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update]
   def index
     @posts = Post.all
   end
@@ -6,14 +7,16 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "つぶやきを投稿しました！"
+    else
+      render :new
+    end
   end
   def edit
-    @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "内容を編集しました！"
     else
@@ -23,5 +26,8 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
